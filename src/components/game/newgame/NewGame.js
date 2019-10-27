84e1997b35a728ca386e1da1e88a9e68122fdae2
FernandaBroch <fernanda.broch@gmail.com>
2019-10-27 16:09:12 -0300

    Create colleagues/backlog with game id close #8

--- src/components/game/newgame/NewGame.js
+++ src/components/game/newgame/NewGame.js
@@ -4,6 +4,8 @@ import { roles } from './Roles';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { newGame } from '../../../store/actions/gameActions'
+import { compose } from 'redux'
+import { firestoreConnect } from 'react-redux-firebase'
 
 class NewGame extends Component{ 
   state = {
@@ -16,30 +18,32 @@ class NewGame extends Component{
 
   handleSubmit = (e) => {
     e.preventDefault();    
-    this.props.newGame(this.state)     
+    const { backlog, colleagues} = this.props
+    this.props.newGame(this.state, backlog, colleagues);
     this.props.history.push('/backlog'); 
   }
     
   render(){
     const { auth, game } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' /> 
+//    console.log(backlog)
+    // if (!auth.uid) return <Redirect to='/signin' /> 
     if (game) return <Redirect to='/backlog' /> 
       return(
         <div className='container'>
           <div className='row white'>    
-          <form onSubmit={this.handleSubmit}>                
-            <div className='col s12 center'><h3>Escolha seu Papel</h3></div>                    
-            {roles.map((role) => (
-              <div className='col s4' key={role.name}>
-                <RoleCard                
-                name={role.name}
-                description={role.description}
-                image={role.image}
-                action={this.handler}
-                />
-              </div>      
-            ))}             
-          </form>
+            <form onSubmit={this.handleSubmit}>                
+              <div className='col s12 center'><h3>Escolha seu Papel</h3></div>                    
+              {roles.map((role) => (
+                <div className='col s4' key={role.name}>
+                  <RoleCard                
+                  name={role.name}
+                  description={role.description}
+                  image={role.image}
+                  action={this.handler}
+                  />
+                </div>      
+              ))}             
+            </form>
           </div>
         </div>
       )
@@ -48,14 +52,26 @@ class NewGame extends Component{
 const mapStateToProps = (state) => {
   return {
     auth: state.firebase.auth,
-    game: state.firebase.game
+    game: state.firebase.game,
+    colleagues: state.firestore.ordered.colleagues,
+    backlog: state.firestore.ordered.backlog
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return{
-    newGame: (game) => dispatch(newGame(game))
+    newGame: (game, backlog, colleagues ) => dispatch(newGame(game, backlog, colleagues))
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(NewGame)
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect([
+    { collection: 'colleagues',
+      where: ['game', '==', '']
+    },
+    { collection: 'backlog',
+      where: ['game', '==', '']
+    },
+  ])
+  )(NewGame)
--- src/store/actions/gameActions.js
+++ src/store/actions/gameActions.js
@@ -1,19 +1,34 @@
 
-export const newGame = (game) => {
+export const newGame = (game, backlog, colleagues) => {
   return (dispatch, getState, {getFirebase, getFirestore}) => {
     const firestore = getFirestore();
     const profile = getState().firebase.profile;
     const uid = getState().firebase.auth.uid;
-
+    
     firestore.collection('games').add({
         ...game,
         nickname: profile.nickname,
         uid: uid,
         createdAt: new Date()
-      }).then(() => {
-      dispatch({ type: 'NEW_GAME_SUCCESS', game });
-    }).catch((err) => {
+      }).then(game => {
+        //create a new backlog list for this game
+        backlog.forEach(bl => {
+          firestore.collection('backlog').add({
+            ...bl,
+            game: game.id        
+          })          
+        })        
+        //create a new team of colleagues for this game
+        colleagues.forEach(col => {
+          firestore.collection('colleagues').add({
+            ...col,
+            game: game.id        
+          })          
+        })            
+        dispatch({ type: 'NEW_GAME_SUCCESS', game });
+      }).catch((err) => {        
       dispatch({ type: 'NEW_GAME_ERROR', err});
     });
+
   }
 }
